9e830fdac67309ccecd2e6318207ecb03eba72e5
phalax4 <jeremylim995@gmail.com>
2015-05-28 12:50:12 -0500

    Fixed imports and indents

--- fys/search.py
+++ fys/search.py
@@ -1,7 +1,9 @@
 import pickle
+import os.path
+
 def search(bible):
-	verses = []
-	if os.path.isfile('bible_data.pkl'):
+    verses = []
+    if (os.path.isfile('bible_data.pkl')):
         with open('bible_data.pkl','wb') as output:
             pickle.dump(bible,output,pickle.HIGHEST_PROTOCOL)
     return verses
