cd01f7c33cedf490cca4187d7ee0c139d95e1c78
phalax4 <jeremylim995@gmail.com>
2015-06-02 22:05:53 -0700

    Adding initDB

--- fys/buildSynonyms.py
+++ fys/buildSynonyms.py
@@ -1,1 +1,21 @@
-import urllib2
+import urllib2,sqlite3,json
+
+baseUrlPrefix = "http://www.thesaurus.com/browse/"
+baseUrlSuffix = "?s=t"
+
+def getSyn(word):
+    pass
+def initDB(bibleSet):
+    conn = sqlite3.connect("synon.db")
+    c = conn.cursor()
+    c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'.format(tn="syn",nf='synon',ft='TEXT'))
+
+if __name__=="__main__":
+	f = open("bible.txt")
+	bibleList = list(set(f))
+	#print bibleList[1]
+	bibleSet = set()
+	for i in bibleList:
+		mylist = (i.split("	")[1]).split()
+		bibleSet |=set(mylist)
+	print len(bibleSet)
