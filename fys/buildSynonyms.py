4551151c8131a7ba7836f4736e114b41c287b4a6
phalax4 <jeremylim995@gmail.com>
2015-06-03 21:51:51 -0700

    Added python3 compatability

--- fys/buildSynonyms.py
+++ fys/buildSynonyms.py
@@ -1,15 +1,23 @@
-import urllib2,sqlite3,json
+import urllib.request,sqlite3,json,string,re
 
 baseUrlPrefix = "http://www.thesaurus.com/browse/"
 baseUrlSuffix = "?s=t"
 
 def getSyn(word):
     pass
-def initDB(bibleSet):
+def insertion(bibleSet):
+	for i in bibleSet:
+		s = list(bibleSet)[i]
+		s = re.sub(r'[^\w\s]','',s)
+		synList = getSyn(s)
+		
+
+def initDB():
     conn = sqlite3.connect("synon.db")
     c = conn.cursor()
-    c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'.format(tn="syn",nf='synon',ft='TEXT'))
-
+    c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'.format(tn="syn",nf='words',ft='TEXT'))
+    conn.commit()
+    conn.close()
 if __name__=="__main__":
 	f = open("bible.txt")
 	bibleList = list(set(f))
@@ -17,5 +25,6 @@ if __name__=="__main__":
 	bibleSet = set()
 	for i in bibleList:
 		mylist = (i.split("	")[1]).split()
-		bibleSet |=set(mylist)
-	print len(bibleSet)
+		bibleSet |= set(mylist)
+	#initDB()
+	#insertion(bibleSet)
