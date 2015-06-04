8f9ee3bbfb217e4da71f8c63dc55fa1218d2fcea
phalax4 <jeremylim995@gmail.com>
2015-06-04 15:27:43 -0700

    Basic Synon Scraping

--- fys/buildSynonyms.py
+++ fys/buildSynonyms.py
@@ -1,20 +1,33 @@
-import urllib.request,sqlite3,json,string,re
+import urllib.request,sqlite3,json,string,re,BeautifulSoup
 
 baseUrlPrefix = "http://www.thesaurus.com/browse/"
 baseUrlSuffix = "?s=t"
 
 def getSyn(word):
-    pass
+	synonList = []
+	with urllib.request.urlopen( baseUrlPrefix+word+baseUrlSuffix):
+		html = response.read()
+		soup = BeautifulSoup(html)
+		ulList = soup.findAll("ul"):
+		result = soup.findAll('ul',{'class':"carouselActivate"})
+		if result.indexOf("no thesaurus results") == -1:
+			return -1
+		for i in ulList:
+			for li in i.findAll('li'):
+				synonList.append(li)
+	return synonList
+
 def insertion(bibleSet):
 	for i in bibleSet:
 		s = list(bibleSet)[i]
-		s = re.sub(r'[^\w\s]','',s)
+		s = re.sub(r'[^\w\s]','',s)#strips off the punctuation
 		synList = getSyn(s)
-		
-
+		if synList == -1:
+			continue
 def initDB():
     conn = sqlite3.connect("synon.db")
     c = conn.cursor()
+    c.execute("DROP TABLE if exists mq")
     c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'.format(tn="syn",nf='words',ft='TEXT'))
     conn.commit()
     conn.close()
